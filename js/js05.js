9287ceed82175c404f58c8bc4a6a68016109aa87
Chi Hao <haopc1404@gmail.com>
2023-08-23 15:05:35 +0700

    addFile

--- js/js05.js
+++ js/js05.js
@@ -1,14 +1,33 @@
 $(document).ready(() => {
     $("#tab01").show();
-    $(".tab a").click(function() {
+    $(".tab li").click(function() {
         $(".picture").hide();
         $(".tab > li").removeClass("active");
-        $(this).parent().addClass("active");
+        $(this).addClass("active");
 
-        let h = $(this).attr("href");
+        let h = $(this).attr("class").replace(" active","");
         $(".tab-content > div").hide();
         $(h).show();
 
         $(h).css("display","flex");
     });
 });
+
+// go to top
+window.onscroll = function(){
+    console.info(document.documentElement.scrollTop);
+    var gototop = document.getElementById("go-to-top");
+    if (document.documentElement.scrollTop > 100 || document.body.scrollTop > 100) {
+      gototop.style.display = "block";
+    } else{
+      gototop.style.display = "none";
+    }
+  }
+  function goToTop(){
+      var timer = setInterval(function(){
+        document.documentElement.scrollTop-=20;
+  
+        if(document.documentElement.scrollTop <= 0)
+          clearInterval(timer);
+      }, 1);
+  };
